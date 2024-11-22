244543fbfc752ec978b5a12c646b9fec7ecb3da9
kevinwww111 <kevinwg114@gmail.com>
2024-11-22 22:11:10 +0800

    button fix

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Movie } from '../types';
+import { Movie } from '../types'; // 导入 Movie 类型
 
 interface MovieListProps {
   movies: Movie[];
@@ -8,19 +8,18 @@ interface MovieListProps {
 
 const MovieList: React.FC<MovieListProps> = ({ movies, onMovieSelect }) => {
   return (
-    <div>
+    <div className="movie-list">
       {movies.length === 0 ? (
-        <p>No upcoming movies found.</p>
+        <p>No upcoming movies available.</p> // 如果没有电影数据，显示此信息
       ) : (
         movies.map((movie) => (
           <div key={movie.id} onClick={() => onMovieSelect(movie)}>
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`}
               alt={movie.title}
-              style={{ width: '200px', height: '300px' }}
+              style={{ width: 200 }}
             />
             <h3>{movie.title}</h3>
-            <p>{movie.releaseDate}</p>
           </div>
         ))
       )}
