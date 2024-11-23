fca78ee6beea350f61ab321ff8d5a10e86795132
kevinwww111 <kevinwg114@gmail.com>
2024-11-23 15:35:52 +0800

    Deploy

---
+++ precache-manifest.2f705eb0d7fd1e999bca0849e4189214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb09615f88188c53238e7eab22c15e1",
+    "url": "/113-1-TKU-midreact/index.html"
+  },
+  {
+    "revision": "6208eaf7439a1b238193",
+    "url": "/113-1-TKU-midreact/static/css/main.f893de28.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/113-1-TKU-midreact/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/113-1-TKU-midreact/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "6208eaf7439a1b238193",
+    "url": "/113-1-TKU-midreact/static/js/main.33b86a8f.chunk.js"
+  },
+  {
+    "revision": "36b2c5ef2656b5940074",
+    "url": "/113-1-TKU-midreact/static/js/runtime~main.d8bb5d93.js"
+  }
+]);
